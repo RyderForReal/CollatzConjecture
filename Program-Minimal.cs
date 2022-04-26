a8c5473af28f36483059936e39c1c3763caa7f0c
RyderForNow <ryder@ryderg.net>
2022-04-26 11:11:41 +0200

    Add minimal version

---
+++ Program-Minimal.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace CollatzConjecture {
+	internal static class ProgramMinimal {
+		public static void Main(string[] args)
+		{
+			var count = 0;
+			uint n = 0;
+			
+			
+			Console.WriteLine("Welcome to the Collatz Conjecture Calculator!");
+			Console.WriteLine();
+			Console.WriteLine("Enter a number (n > 1): ");
+			n = uint.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+				
+			if (n <= 1) { Console.WriteLine("Invalid number! Press any key to exit."); Console.ReadKey(); return; }
+
+			// Actual program logic
+			while (n != 1)
+			{
+				if (n % 2 == 0)
+				{
+					n /= 2;
+				}
+				else
+				{
+					n = 3 * n + 1;
+				}
+
+				count++;
+			}
+
+			Console.WriteLine();
+			Console.WriteLine($"It took {count + 1} steps to reach 1.");
+			Console.WriteLine();
+			Console.WriteLine("Press enter to close the program.");
+			Console.ReadLine();
+		}
+	}
+}
